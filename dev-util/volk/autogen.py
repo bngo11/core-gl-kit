4610c63d9e32fc8b57c6f45de6290f8613f48842
shadow <shadowngoz@gmail.com>
2025-04-04 15:05:15 -0500

    Autogenerated tree updates.
    merged:
    
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      kit-fixups: e7a37beaacc66d2c45961ea4b7fb7a344ddb9a86

--- dev-util/volk/autogen.py
+++ dev-util/volk/autogen.py
@@ -31,7 +31,7 @@ async def generate(hub, **pkginfo):
 
 	if version and url:
 	'''
-	version = "1.4.304"
+	version = "1.4.309"
 	url = f"https://github.com/zeux/volk/archive/vulkan-sdk-{version}.tar.gz"
 	fn = f"volk-{version}.tar.gz"
 	ebuild = hub.pkgtools.ebuild.BreezyBuild(
