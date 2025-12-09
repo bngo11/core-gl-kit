16168d47886a644434f4f716d61892b4b620ba04
shadow <shadowngoz@gmail.com>
2025-12-08 22:09:22 -0600

    Autogenerated tree updates.
    merged:
    
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      kit-fixups: ade369b28e36f14076c43557e278a65fdcf0e1af

--- dev-util/volk/autogen.py
+++ dev-util/volk/autogen.py
@@ -3,35 +3,7 @@
 import json
 
 async def generate(hub, **pkginfo):
-	'''
-	json_data = await hub.pkgtools.fetch.get_page("https://api.github.com/repos/mesonbuild/meson/releases", is_json=True)
-	version = None
-	url = None
-
-	for item in json_data:
-		try:
-			if item["prerelease"] or item["draft"]:
-				continue
-
-			version = item["tag_name"]
-			list(map(int, version.split(".")))
-
-			for asset in item['assets']:
-				asset_name = asset["name"]
-
-				if asset_name.endswith("tar.gz"):
-					url = asset["browser_download_url"]
-					break
-
-			if url:
-				break
-
-		except (KeyError, IndexError, ValueError):
-			continue
-
-	if version and url:
-	'''
-	version = "1.4.309"
+	version = "1.4.328"
 	url = f"https://github.com/zeux/volk/archive/vulkan-sdk-{version}.tar.gz"
 	fn = f"volk-{version}.tar.gz"
 	ebuild = hub.pkgtools.ebuild.BreezyBuild(
