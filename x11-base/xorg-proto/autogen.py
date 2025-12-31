afbf00a46acd53762a85c0f1b0f8dc99e577dda1
shadow <shadowngoz@gmail.com>
2025-12-30 23:08:58 -0600

    Autogenerated tree updates.
    merged:
    
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      kit-fixups: 121ef570be9786fb2695da22499bcc838cda42fe

--- x11-base/xorg-proto/autogen.py
+++ x11-base/xorg-proto/autogen.py
@@ -191,7 +191,7 @@ src_install() { return 0; }
 			name=pv_key[0],
 			cat="x11-proto",
 			version=pv_key[1],
-			revision=2,
+			revision=3,
 			all_meta_atoms=all_meta_atoms,
 			template_text=sub_ebuild_template,
 		)
