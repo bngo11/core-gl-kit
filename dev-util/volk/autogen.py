4b85288e4ec08460f9c43deda3c435abb00cec0c
shadow <shadowngoz@gmail.com>
2026-02-04 17:00:40 -0600

    Autogenerated tree updates.
    merged:
    
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      kit-fixups: 6d23fe0495c322b1ccb948e2c7f9c51a542313d3

--- dev-util/volk/autogen.py
+++ dev-util/volk/autogen.py
@@ -3,14 +3,29 @@
 import json
 
 async def generate(hub, **pkginfo):
-	version = "1.4.328"
-	url = f"https://github.com/zeux/volk/archive/vulkan-sdk-{version}.tar.gz"
-	fn = f"volk-{version}.tar.gz"
-	ebuild = hub.pkgtools.ebuild.BreezyBuild(
-		**pkginfo,
-		version=version,
-		artifacts=[hub.pkgtools.ebuild.Artifact(url=url, final_name=fn)]
-	)
-	ebuild.push()
+	# get version
+	json_data = await hub.pkgtools.fetch.get_page(f"https://api.github.com/repos/KhronosGroup/Vulkan-Headers/tags", is_json=True)
+
+	for item in json_data:
+		try:
+			name = item["name"]
+			if not name.startswith("vulkan-sdk-"):
+				continue
+			version = name.rsplit("-", 1)[-1]
+			list(map(int, version.split(".")))
+			break
+
+		except (KeyError, IndexError, ValueError):
+			continue
+
+	if version:
+		url = f"https://github.com/zeux/volk/archive/vulkan-sdk-{version}.tar.gz"
+		fn = f"volk-{version}.tar.gz"
+		ebuild = hub.pkgtools.ebuild.BreezyBuild(
+			**pkginfo,
+			version=version,
+			artifacts=[hub.pkgtools.ebuild.Artifact(url=url, final_name=fn)]
+		)
+		ebuild.push()
 
 # vim: ts=4 sw=4 noet
